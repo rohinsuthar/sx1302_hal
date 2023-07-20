19c34bb043c2239fa385874dc7881324c78c50fc
rohinsuthar <133192405+rohinsuthar@users.noreply.github.com>
2023-07-20 16:50:39 +0530

    Create beacon_frame_info.h

---
+++ packet_forwarder/src/beacon_frame_info.h
@@ -0,0 +1,13 @@
+// beacon_frame_info.h
+
+#ifndef BEACON_FRAME_INFO  // Include guards to prevent multiple inclusions
+#define BEACON_FRAME_INFO
+
+typedef struct beaconframeinfo{
+uint32_t BEACON_GUARD = 3000000; /* Interval where no ping slot can be placed,
+                                            to ensure beacon can be sent */
+uint32_t BEACON_RESERVED = 2120000;/* Time on air of the beacon, with some margin */
+}bconfinfo_t;
+
+
+#endif // MY_HEADER_H
