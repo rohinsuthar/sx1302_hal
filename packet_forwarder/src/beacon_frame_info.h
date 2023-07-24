6d5907b13ca05c93ba2738a023ba8206b5d6682d
rohinsuthar <133192405+rohinsuthar@users.noreply.github.com>
2023-07-25 02:27:27 +0530

    Update beacon_frame_info.h

--- packet_forwarder/src/beacon_frame_info.h
+++ packet_forwarder/src/beacon_frame_info.h
@@ -5,7 +5,7 @@
 
 #include <stdint.h>  
 
-typedef struct beaconframeinfo{
+typedef struct sbconfinfo{
 uint32_t BEACON_GUARD = 3000000; /* Interval where no ping slot can be placed,
                                             to ensure beacon can be sent */
 uint32_t BEACON_RESERVED = 2120000;/* Time on air of the beacon, with some margin */
